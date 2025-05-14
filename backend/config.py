0d9a092fcfb2cc2935fa939bda3e147494e8796b
kaleper <kalenperedo3@gmail.com>
2025-05-14 16:39:39 -0600

    Add config information Create app in config.py. Configure SQLALCHEMY, creating a db instance

---
+++ backend/config.py
@@ -0,0 +1,13 @@
+from flask import flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+#Enable CORS 
+CORS(app)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mydatabase.db"
+# Disable change log to DB for simplicity
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
