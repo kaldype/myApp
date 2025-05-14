8e1040a006820c5feaeb2decbbd95ab13ff512be
kaleper <kalenperedo3@gmail.com>
2025-05-14 16:48:18 -0600

    Create contact class for database Database model represented as python class. Added fields for Contact model and function to convert data to json

---
+++ backend/models.py
@@ -0,0 +1,19 @@
+from config import db
+
+# Database model represented as a python class via ORM
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # Max length is 80 chars
+    first_name = db.Column(db.String(80), unique = False, nullable = False)
+    last_name = db.Column(db.String(80), unique = False, nullable = False)
+    e-mail = db.Column(db.String(120), unique = True, nullable = False)
+
+    # API will receive and send JSON contact info
+    def to_json(self):
+        return {
+            "id": self.id,
+            "firstName": self.first_name,
+            "lastName": self.last_name,
+            "email": self.email,
+        }
+        
